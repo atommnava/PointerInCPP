51cd5a6a0458a1c9e1b2e914bcb750a8ec999db6
atommnava <atommunoznava@gmail.com>
2026-04-21 15:45:42 -0600

    Intro

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n = 5;
+    int *ptr = &n;
+    cout << &n << endl;
+    cout << ptr << endl;
+    cout << *ptr << endl;
+    return 0;
+}
+
