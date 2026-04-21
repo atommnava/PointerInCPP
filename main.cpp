a0935530750705ac7a304d9e87185ed5d6f341b5
atommnava <atommunoznava@gmail.com>
2026-04-21 16:00:11 -0600

    Intro

--- main.cpp
+++ main.cpp
@@ -12,6 +12,14 @@ int main(){
     *ptr = 10;
     cout << *ptr << endl;
     cout << n << endl;
+
+    int v;
+    int *ptr2 = &v;
+    *ptr2 = 7;
+    cout << "v = " << v << endl;
+    cout << "*ptr2 = " << *ptr2 << endl;
+    cout << "ptr2 = " << ptr2 << endl;
+
     return 0;
 }
 
