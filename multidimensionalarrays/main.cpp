f08e6d20ecfd1bdccfd16fd272c1d7eed90ee08d
atommnava <atommunoznava@gmail.com>
2026-04-22 20:05:20 -0600

    Multidimensional Array

--- multidimensionalarrays/main.cpp
+++ multidimensionalarrays/main.cpp
@@ -3,5 +3,14 @@
 using namespace std;
 
 int main(void){
+    int rows, cols;
+    cout << "Enter Rows And Cols: " << endl;
+    cin >> rows >> cols;
+    int **table = new int*[rows];
+
+    for (int i = 0; i < rows; i++) {
+        table[i] = new int[cols];
+    }
+
     return 0;
 }
